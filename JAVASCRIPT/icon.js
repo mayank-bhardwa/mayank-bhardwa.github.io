fe1046c0e437882360bd4c0e4b942a2429bee8f1
Mayank Bhardwaj <mayank85277@gmail.com>
2020-09-16 00:00:16 +0530

    Navigation Bar Complete
    Responsive navigation bar made

---
+++ JAVASCRIPT/icon.js
@@ -0,0 +1,10 @@
+function dropMenu()
+{
+    var x = document.getElementById("right-nav");
+    if (x.className == "right-nav-bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "right-nav-bar";
+    }
+    console.log(x.className);
+}
--- intro.js
+++
@@ -1,2 +0,0 @@
-for(let index=0;index<5;index++)
-    alert("Website coming soon");
